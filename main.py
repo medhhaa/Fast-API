535e4650d9449426cad44e0a90f48e8a76ac3e3a
Medha Khatri <81421540+medhhaa@users.noreply.github.com>
2025-07-16 17:01:30 -0400

    get a specific and all todos

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 
+# Fastapi runs on Scarlet, so by default all methods would be asynchronous, but you can still decide which endpoints make sense to be async-await and synchronous
 
 api = FastAPI()
+
+todos[] = [
+    {'todo_id': 1, 'todo_name': 'Buy groceries', 'todo_description': 'Milk, Bread, Eggs'},
+    {'todo_id': 2, 'todo_name': 'Walk the dog', 'todo_description': 'Take the dog for a walk in the park'},
+    {'todo_id': 3, 'todo_name': 'Read a book', 'todo_description': 'Finish reading "The Great Gatsby"'},
+    {'todo_id': 4, 'todo_name': 'Exercise', 'todo_description': 'Go for a run or hit the gym'},
+    {'todo_id': 5, 'todo_name': 'Clean the house', 'todo_description': 'Vacuum and dust all rooms'},
+]
 
 # GET, POST, PUT DELETE
 # GET - Retrieve information
@@ -11,4 +20,8 @@ api = FastAPI()
 @api.get("/")
 def index():
     return {"message": "Hello, World!"}
+
+@api.get('/calculation')
+def calculation():
+
 
