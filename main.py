1d898a05cc4fd925d63c1ed8013ce926603e8073
Medha Khatri <81421540+medhhaa@users.noreply.github.com>
2025-07-17 14:20:47 -0400

    Raise HTTPExceptions

--- main.py
+++ main.py
@@ -15,7 +15,7 @@
 from enum import IntEnum
 from typing import Optional, List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 
 
@@ -35,17 +35,29 @@ class TodoBase(BaseModel):
     # The description is also important. This is not just some documenation, but it is enforced, 
     # When something inherits from TodoBase, then it has to fit this requirement. 
     todo_name: str = Field(..., description="The name of the todo item", min_length = 3, max_length = 512)
+    todo_description: str = Field(..., description="The description of the todo item")
     priority: Priority = Field(default=Priority.LOW, description="The priority of the todo item, can be LOW, MEDIUM, or HIGH")
 
+class TodoCreate(TodoBase):
+    # It has no major difference from TodoBase, except that it is used for creating a new todo item.
+    pass
 
+class Todo(TodoBase):
+    todo_id: int = Field(..., description="The unique identifier for the todo item") 
+    pass
+
+class TodoUpdate(BaseModel):
+    todo_name: Optional[str] = Field(None, description="The name of the todo item", min_length = 3, max_length = 512)
+    todo_description: Optional[str] = Field(None, description="The description of the todo item", min_length = 3, max_length = 1024)
+    priority: Optional[Priority] = Field(None, description="The priority of the todo item, can be LOW, MEDIUM, or HIGH")
 
 
 all_todos = [
-    {'todo_id': 1, 'todo_name': 'Buy groceries', 'todo_description': 'Milk, Bread, Eggs'},
-    {'todo_id': 2, 'todo_name': 'Walk the dog', 'todo_description': 'Take the dog for a walk in the park'},
-    {'todo_id': 3, 'todo_name': 'Read a book', 'todo_description': 'Finish reading "The Great Gatsby"'},
-    {'todo_id': 4, 'todo_name': 'Exercise', 'todo_description': 'Go for a run or hit the gym'},
-    {'todo_id': 5, 'todo_name': 'Clean the house', 'todo_description': 'Vacuum and dust all rooms'},
+    Todo(todo_id = 1, todo_name = "Buy groceries", todo_description = "Milk, Bread, Eggs", priority = Priority.HIGH),
+    Todo(todo_id = 2, todo_name = "Walk the dog", todo_description = "Take the dog for a walk in the park", priority = Priority.MEDIUM),     
+    Todo(todo_id = 3, todo_name = "Read a book", todo_description = "Finish reading 'The Great Gatsby'", priority = Priority.LOW),
+    Todo(todo_id = 4, todo_name = "Exercise", todo_description = "Go for a run or hit the gym", priority = Priority.MEDIUM),
+    Todo(todo_id = 5, todo_name = "Clean the house", todo_description = "Vacuum and dust all rooms", priority = Priority.LOW)
 ]
 
 # GET, POST, PUT DELETE
@@ -53,20 +65,17 @@ all_todos = [
 # POST - Add new information
 # PUT - Update existing information
 # DELETE - Remove information
-@api.get("/")
-def index():
-    return {"message": "Hello, World!"}
 
 # Path parameter
-@api.get('/todos/{todo_id}')
+@api.get('/todos/{todo_id}', response_model=Todo)
 def get_todo(todo_id: int):
     for todo in all_todos:
-        if todo['todo_id'] == todo_id:
+        if todo.todo_id == todo_id:
             return todo
-    return {"error": "Todo not found"}, 404
+    raise HTTPException(status_code = 404, detail = "Todo not found")
 
 # Query parameter: localhost:9999/todos?first_n=2
-@api.get('/todos')
+@api.get('/todos', response_model=List[Todo])
 def get_all_todos(first_n: int = None):
     if first_n is None:
         return all_todos
@@ -80,39 +89,46 @@ def get_all_todos(first_n: int = None):
 # You can access it at http://localhost:9999/docs
 
 # POST method to create a new todo
-@api.post('/todos')
-def create_todo(todo: dict):
-    new_todo_id = max(todo['todo_id'] for todo in all_todos) + 1 
 
-    new_todo = {
-        'todo_id': new_todo_id,
-        'todo_name': todo['todo_name'],
-        'todo_description': todo['todo_description']
-    }
+# response model is essentially, what are you returning
+# but error strings may not be the same as your response model
+# so you raise HTTPExceptions instead
+
+# Typing
+@api.post('/todos', response_model=Todo)
+def create_todo(todo: TodoCreate):
+    new_todo_id = max(todo.todo_id for todo in all_todos) + 1 
+
+    new_todo = Todo(
+        todo_id=new_todo_id,
+        todo_name=todo.todo_name,
+        todo_description=todo.todo_description,
+        priority=todo.priority
+    )
 
     all_todos.append(new_todo)
 
     return new_todo
 
 # PUT method to update an existing todo
-@api.put('/todos/{todo_id}')
-def update_todo(todo_id: int, updated_todo: dict):
+@api.put('/todos/{todo_id}', response_model=Todo)
+def update_todo(todo_id: int, updated_todo: TodoUpdate):
     for todo in all_todos:
-        if todo['todo_id'] == todo_id:
-            todo['todo_name'] = updated_todo.get('todo_name', todo['todo_name'])
-            todo['todo_description'] = updated_todo.get('todo_description', todo['todo_description'])
+        if todo.todo_id == todo_id:
+            todo.todo_name = updated_todo.todo.todo_name
+            todo['todo_description'] = updated_todo.todo_description
             return todo
-    return {"error": "Todo not found"}, 404
+    raise HTTPException(status_code = 404, detail = "Todo not found")
 
 # DELETE method to remove a todo
-@api.delete('/todos/{todo_id}')
+@api.delete('/todos/{todo_id}', response_model=Todo)
 def delete_todo(todo_id: int):
     for index, todo in enumerate(all_todos):
-        if todo['todo_id'] == todo_id:
+        if todo.todo_id == todo_id:
             deleted_todo = all_todos.pop(index)
             return {"message": "Todo deleted successfully"}
     # If the todo_id is not found, return an error message
-    return "Error, not found"
+    raise HTTPException(status_code = 404, detail = "Todo not found")
    
 # Run FastAPI with uvicorn
 
